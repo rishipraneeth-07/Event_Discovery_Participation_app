fc34b88029f0442679800fc65811fa8ef19e2e47
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-03-10 16:25:56 +0530

    Added User class

---
+++ src/main/java/com/college/eventapp/model/User.java
@@ -0,0 +1,25 @@
+package com.college.eventapp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false,length = 100)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
