f75c9dbf9f644805862a54ddc10ad5b66f798993
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-03-16 19:29:49 +0530

    Registration Service interface

---
+++ src/main/java/com/college/eventapp/service/RegistrationService.java
@@ -0,0 +1,11 @@
+package com.college.eventapp.service;
+
+import com.college.eventapp.model.Registration;
+
+import java.util.List;
+
+public interface RegistrationService {
+    Registration registerForEvent(Long userId, Long eventId);
+    List<Registration> getUserRegistrations(Long userId);
+    List<Registration> getEventRegistrations(Long eventId);
+}
