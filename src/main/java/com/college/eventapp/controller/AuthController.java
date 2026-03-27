810736324f76b37f67cd3029c359dbe513364fe1
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-03-27 18:50:28 +0530

    Updated Auth Controller

--- src/main/java/com/college/eventapp/controller/AuthController.java
+++ src/main/java/com/college/eventapp/controller/AuthController.java
@@ -1,13 +1,10 @@
 package com.college.eventapp.controller;
 
 import com.college.eventapp.dto.AuthRequestDTO;
-import com.college.eventapp.dto.UserResponseDTO;
+import com.college.eventapp.dto.AuthResponseDTO;
 import com.college.eventapp.model.User;
 import com.college.eventapp.service.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -20,36 +17,30 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public UserResponseDTO register(@RequestBody AuthRequestDTO request) {
-
+    public AuthResponseDTO register(@RequestBody AuthRequestDTO request) {
         User user = new User();
         user.setName(request.getName());
         user.setEmail(request.getEmail());
         user.setPassword(request.getPassword());
         user.setRole(request.getRole());
 
-        User savedUser = userService.registerUser(user);
-
-        UserResponseDTO response = new UserResponseDTO();
-        response.setId(savedUser.getId());
-        response.setName(savedUser.getName());
-        response.setEmail(savedUser.getEmail());
-        response.setRole(savedUser.getRole());
-
-        return response;
+        User saved = userService.registerUser(user);
+        return buildAuthResponse(saved);
     }
 
     @PostMapping("/login")
-    public UserResponseDTO login(@RequestBody AuthRequestDTO request) {
-
+    public AuthResponseDTO login(@RequestBody AuthRequestDTO request) {
         User user = userService.loginUser(request.getEmail(), request.getPassword());
+        return buildAuthResponse(user);
+    }
 
-        UserResponseDTO response = new UserResponseDTO();
-        response.setId(user.getId());
+    private AuthResponseDTO buildAuthResponse(User user) {
+        AuthResponseDTO response = new AuthResponseDTO();
+        response.setToken("user-" + user.getId());
+        response.setUserId(user.getId());
         response.setName(user.getName());
         response.setEmail(user.getEmail());
         response.setRole(user.getRole());
-
         return response;
     }
 }
