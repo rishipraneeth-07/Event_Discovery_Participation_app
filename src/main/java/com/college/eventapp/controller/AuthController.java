cc1f2564660c693b488f4b1a08fc29e4ed5de0dd
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-04-09 21:19:43 +0530

    Updated authcontroller

--- src/main/java/com/college/eventapp/controller/AuthController.java
+++ src/main/java/com/college/eventapp/controller/AuthController.java
@@ -2,11 +2,14 @@ package com.college.eventapp.controller;
 
 import com.college.eventapp.dto.AuthRequestDTO;
 import com.college.eventapp.dto.AuthResponseDTO;
+import jakarta.validation.Valid;
 import com.college.eventapp.model.User;
 import com.college.eventapp.service.UserService;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Validated
 @RequestMapping("/api/auth")
 public class AuthController {
 
@@ -17,7 +20,7 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public AuthResponseDTO register(@RequestBody AuthRequestDTO request) {
+    public AuthResponseDTO register(@Valid @RequestBody AuthRequestDTO request) {
         User user = new User();
         user.setName(request.getName());
         user.setEmail(request.getEmail());
@@ -29,9 +32,34 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public AuthResponseDTO login(@RequestBody AuthRequestDTO request) {
+    public AuthResponseDTO login(@Valid @RequestBody LoginRequest request) {
         User user = userService.loginUser(request.getEmail(), request.getPassword());
         return buildAuthResponse(user);
+    }
+
+    public static class LoginRequest {
+        @jakarta.validation.constraints.NotBlank(message = "Email is required")
+        @jakarta.validation.constraints.Email(message = "Email must be valid")
+        private String email;
+
+        @jakarta.validation.constraints.NotBlank(message = "Password is required")
+        private String password;
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
     }
 
     private AuthResponseDTO buildAuthResponse(User user) {
