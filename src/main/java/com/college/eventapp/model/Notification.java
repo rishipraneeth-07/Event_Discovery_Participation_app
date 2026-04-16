da3db1e1043ee1f57929d9f1cf1a4469b378fc12
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-04-16 21:28:07 +0530

    added new methods and validation

--- src/main/java/com/college/eventapp/model/Notification.java
+++ src/main/java/com/college/eventapp/model/Notification.java
@@ -16,13 +16,22 @@ public class Notification {
     @Column(nullable = false)
     private String message;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "created_at")
     private LocalDateTime createdAt;
 
-    @Column(nullable = false,name = "is_read")
+    @Column(nullable = false, name = "is_read")
     private boolean read;
 
+    @Column(nullable = false, name = "isread")
+    private boolean legacyRead;
+
     @ManyToOne
-    @JoinColumn(name = "user_id",nullable = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @PrePersist
+    @PreUpdate
+    void syncLegacyReadColumn() {
+        this.legacyRead = this.read;
+    }
 }
