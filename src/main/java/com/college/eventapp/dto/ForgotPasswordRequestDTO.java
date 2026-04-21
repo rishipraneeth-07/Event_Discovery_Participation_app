96546e14b171d6c54bfaad4b5ef383b22463ef75
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-04-21 23:03:36 +0530

    added forgot password response dto

---
+++ src/main/java/com/college/eventapp/dto/ForgotPasswordRequestDTO.java
@@ -0,0 +1,12 @@
+package com.college.eventapp.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class ForgotPasswordRequestDTO {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+}
