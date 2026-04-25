4ef9ba282b4655e913e43e0a53120bd09888056d
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-04-26 00:06:01 +0530

    added new exception class

---
+++ src/main/java/com/college/eventapp/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.college.eventapp.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
