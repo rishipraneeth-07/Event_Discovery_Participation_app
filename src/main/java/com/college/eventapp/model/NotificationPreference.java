b4782afb52f92f47295e40fbd41ccbfcf4df55a9
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-04-28 00:23:21 +0530

    Added new entity

---
+++ src/main/java/com/college/eventapp/model/NotificationPreference.java
@@ -0,0 +1,28 @@
+package com.college.eventapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "notification_preferences")
+@Data
+public class NotificationPreference {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    private boolean eventApproval = true;
+    private boolean registrationUpdates = true;
+    private boolean reminders = true;
+    private boolean marketing = false;
+}
