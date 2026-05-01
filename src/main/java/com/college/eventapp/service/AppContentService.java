71a9778223f2f1f4754dcdc6d32dd616caad70d2
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-05-02 04:16:10 +0530

    added App COntents Service class

---
+++ src/main/java/com/college/eventapp/service/AppContentService.java
@@ -0,0 +1,7 @@
+package com.college.eventapp.service;
+
+import com.college.eventapp.dto.AppContentResponseDTO;
+
+public interface AppContentService {
+    AppContentResponseDTO getAppContent();
+}
