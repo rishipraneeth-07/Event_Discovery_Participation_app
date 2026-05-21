a19fb731b8000efa64e0976e0e02c0c065f262e4
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-05-21 21:52:07 +0530

    updated authrequest dto

--- src/main/java/com/college/eventapp/dto/AuthRequestDTO.java
+++ src/main/java/com/college/eventapp/dto/AuthRequestDTO.java
@@ -18,7 +18,7 @@ public class AuthRequestDTO {
     private String email;
 
     @NotBlank(message = "Password is required")
-    @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
+    @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters")
     private String password;
 
     @NotNull(message = "Role is required")
