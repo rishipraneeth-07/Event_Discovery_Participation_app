6930a28debd12ab9ddbf0c12b8a6c529828da7aa
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-05-22 23:28:55 +0530

    Updated EventAnalyticsResponse dto

--- src/main/java/com/college/eventapp/dto/EventAnalyticsResponseDTO.java
+++ src/main/java/com/college/eventapp/dto/EventAnalyticsResponseDTO.java
@@ -5,12 +5,10 @@ import lombok.Data;
 @Data
 public class EventAnalyticsResponseDTO {
     private Long eventId;
-    private long registrations;
-    private int capacity;
-    private int occupancyPercent;
+    private String title;
+    private int totalCapacity;
+    private long registeredCount;
+    private int availableSpots;
+    private double fillRate;
     private String status;
-    private String category;
-    private long views;
-    private long savedCount;
-    private long notificationCount;
 }
