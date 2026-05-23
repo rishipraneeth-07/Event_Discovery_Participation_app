508ee1c9eb5e335e39f89da3b047dc66c7ba3ec8
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-05-23 23:00:10 +0530

    Updated regist response dto

--- src/main/java/com/college/eventapp/dto/RegistrationResponseDTO.java
+++ src/main/java/com/college/eventapp/dto/RegistrationResponseDTO.java
@@ -2,11 +2,14 @@ package com.college.eventapp.dto;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class RegistrationResponseDTO {
     private Long id;
     private Long eventId;
     private String eventTitle;
+    private LocalDateTime eventDateTime;
     private String eventDate;
     private String eventLocation;
     private Long userId;
