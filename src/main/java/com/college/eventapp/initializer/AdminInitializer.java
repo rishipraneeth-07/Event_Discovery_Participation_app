3712d791ec0a3d222e2039639985bc31cffbfe0f
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-05-23 23:03:54 +0530

    added pass encoder in admin init

--- src/main/java/com/college/eventapp/initializer/AdminInitializer.java
+++ src/main/java/com/college/eventapp/initializer/AdminInitializer.java
@@ -6,22 +6,25 @@ import com.college.eventapp.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AdminInitializer {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public AdminInitializer(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Bean
     CommandLineRunner createAdmin(UserRepository userRepository) {
         return args -> {
             if (userRepository.findByEmail("admin@event.com").isEmpty()) {
-
-                BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
                 User admin = new User();
                 admin.setName("Admin");
                 admin.setEmail("admin@event.com");
-                admin.setPassword(encoder.encode("admin123"));
+                admin.setPassword(passwordEncoder.encode("admin123"));
                 admin.setRole(Role.valueOf("ADMIN"));
                 userRepository.save(admin);
 
