ee2dbd1d934c9438a285beb079f12cbf8e7074d5
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-05-26 15:30:55 +0530

    Added Notification repo

--- src/main/java/com/college/eventapp/repository/NotificationRepository.java
+++ src/main/java/com/college/eventapp/repository/NotificationRepository.java
@@ -3,13 +3,20 @@ package com.college.eventapp.repository;
 import com.college.eventapp.model.Notification;
 import com.college.eventapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    List<Notification> findByUser(User user);
+    List<Notification> findByUserIdOrderByCreatedAtDesc(Long userId);
     List<Notification> findByUserAndRead(User user, boolean read);
     boolean existsByUserAndRead(User user, boolean read);
+    long countByMessageContainingIgnoreCase(String message);
+    @Modifying
+    @Query("update Notification n set n.read = true where n.user.id = :userId and n.read = false")
+    int markAllReadByUserId(@Param("userId") Long userId);
 }
