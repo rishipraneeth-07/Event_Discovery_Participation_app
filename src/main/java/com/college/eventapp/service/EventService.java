b755f468c35e41a2ef762e94c8c262068c164cc6
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-06-01 22:10:27 +0530

    Added event methods

--- src/main/java/com/college/eventapp/service/EventService.java
+++ src/main/java/com/college/eventapp/service/EventService.java
@@ -1,7 +1,9 @@
 package com.college.eventapp.service;
+
+import com.college.eventapp.dto.EventAnalyticsResponseDTO;
+import com.college.eventapp.model.Event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import com.college.eventapp.model.Event;
 
 import java.util.List;
 
@@ -9,12 +11,15 @@ public interface EventService {
     Event createEvent(Event event);
     Event updateEvent(Long id, Event event);
     void deleteEvent(Long id);
+    Event cancelEvent(Long id);
+    Event duplicateEvent(Long id);
+    EventAnalyticsResponseDTO getEventAnalytics(Long id);
     Event getEventById(Long id);
     List<Event> getAllEvents();
     List<Event> getApprovedEvents();
     List<Event> getEventsByOrganizer(Long organizerId);
-    Event approveEvent(Long eventId);
-    Event rejectEvent(Long eventId);
+    Event approveEvent(Long eventId, String note);
+    Event rejectEvent(Long eventId, String note);
     Page<Event> getAllEventsPaged(Pageable pageable);
     Page<Event> getApprovedEventsPaged(Pageable pageable);
     Page<Event> searchEventsPaged(String keyword, Pageable pageable);
