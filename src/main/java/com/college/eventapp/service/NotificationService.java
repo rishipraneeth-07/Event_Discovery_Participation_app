d83ade500d585ede3156e468bd9a47d556aa846e
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-06-02 21:37:52 +0530

    updated nOtification methods

--- src/main/java/com/college/eventapp/service/NotificationService.java
+++ src/main/java/com/college/eventapp/service/NotificationService.java
@@ -1,11 +1,16 @@
 package com.college.eventapp.service;
 
+import com.college.eventapp.dto.NotificationResponseDTO;
 import com.college.eventapp.model.Notification;
 
 import java.util.List;
 
 public interface NotificationService {
-    Notification createNotification(Long userId, String message);
-    List<Notification> getUserNotifications(Long userId);
-    void markAsRead(Long notificationId);
+    Notification createNotification(Long userId, String message, String type);
+    Notification createNotification(Long userId, String message, String type, String preferenceType);
+    Notification createMandatoryNotification(Long userId, String message, String type);
+    List<NotificationResponseDTO> getUserNotifications(Long userId);
+    List<NotificationResponseDTO> getCurrentUserNotifications();
+    NotificationResponseDTO markAsRead(Long notificationId);
+    void markAllReadForCurrentUser();
 }
